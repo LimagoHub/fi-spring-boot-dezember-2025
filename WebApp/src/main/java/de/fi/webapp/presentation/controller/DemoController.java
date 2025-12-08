e0c4d2822a8748286472068a876df31ce9ba1ee3
Jo Wagner <jowagner@limago.de>
2025-12-08 16:49:36 +0100

    letzter stand Montag Abend

--- WebApp/src/main/java/de/fi/webapp/presentation/controller/DemoController.java
+++ WebApp/src/main/java/de/fi/webapp/presentation/controller/DemoController.java
@@ -1,12 +1,18 @@
-package de.fi.webapp.presentation;
+package de.fi.webapp.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+
+
 
     @GetMapping("/gruss")
     public String gruss() {
---
+++ WebApp/src/main/java/de/fi/webapp/presentation/controller/PersonenController.java
@@ -0,0 +1,42 @@
+package de.fi.webapp.presentation.controller;
+
+
+import de.fi.webapp.presentation.dto.PersonDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/personen")
+public class PersonenController {
+
+    @Operation(summary = "Liefert eine Person")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Person gefunden",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "ungueltige ID",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Person nicht gefunden",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "internal server error",
+                    content = @Content)})
+
+    @GetMapping(path="/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<PersonDTO> findPersonById(@PathVariable UUID id) {
+        if(id.toString().endsWith("7")) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new PersonDTO(UUID.randomUUID(), "John", "Doe"));
+    }
+}
