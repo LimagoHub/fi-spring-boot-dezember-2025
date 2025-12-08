2e335fca8b33ce0e53a9be5275b9e1bb3c3f09df
Jo Wagner <jowagner@limago.de>
2025-12-08 14:18:28 +0100

    webapp

--- SimpleSpring/src/main/java/de/fi/simplespring/Demo.java
+++ SimpleSpring/src/main/java/de/fi/simplespring/Demo.java
@@ -7,6 +7,7 @@ import jakarta.annotation.PreDestroy;
 import jakarta.inject.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -22,22 +23,27 @@ import org.springframework.stereotype.Component;
 public class Demo {
     // 2. Danach der Inject
 
+
+    private final String gruss ;
+
     private final Translator translator;
 
 
 
     // 1. Aufruf Konstruktor
 
-    @Autowired
-    public Demo( final Translator translator) {
+    //@Autowired
+    public Demo( final Translator translator, @Value("${Demo.gruss}") final String gruss) {
         this.translator = translator;
+        this.gruss = gruss;
         System.out.println(translator.translate("Ctor von Demo"));
+        System.out.println(translator.translate(gruss));
     }
 
     // 3. Nach Inject
     @PostConstruct
     public void play() {
-        System.out.println(translator.translate("Hello World"));
+        System.out.println(translator.translate(gruss));
     }
 
     @PreDestroy // ACHTUNG nur bei scope singleton
